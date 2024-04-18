442f762b813e288b36c20c6f2c30b6b886e1244a
Leonel Diaz <156236211+Leosdelrey1111@users.noreply.github.com>
2024-04-17 23:36:22 -0600

    Add files via upload

---
+++ SESION20/app/src/main/java/mx/utng/sesion20/Reto1.java
@@ -0,0 +1,12 @@
+package mx.utng.sesion20;
+
+public class Reto1 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 1; i <= 100; i++) {
+            suma += i;
+        }
+        System.out.println("la sum de los primeros 100 numeros es: "+suma);
+    }
+    
+}
